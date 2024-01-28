df9e0b92365e63ef4b671b127a9742f0fda38971
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-28 18:39:25 +0100

    add Vue & add dinamic images

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@ console.log('Vue OK', Vue);
 
 // Attivo Vue
 const { createApp } = Vue;
-const app = createApp({});
+const app = createApp({
+    data() {
+        return {
+            destinations,
+            activeIndex: 0
+        }
+    },
+    computed: {},
+    methods: {}
+});
 app.mount('#root');
